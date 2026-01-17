f19d1a0de2457f26f391fb2e69ca1cb73e97d07e
Juan Medina <jamedina@gmail.com>
2026-01-17 10:58:00 +0000

    Add main.hpp with PXE_MAIN macro and run_app template
    Introduces include/pxe/main.hpp, providing a run_app template function for running PXE applications with error handling and logging. Adds the PXE_MAIN macro to define main and WinMain entry points, supporting both standard and Windows-specific application entry.

---
+++ include/pxe/main.hpp
@@ -0,0 +1,68 @@
+// SPDX-FileCopyrightText: 2026 Juan Medina
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <pxe/app.hpp>
+
+#ifndef __EMSCRIPTEN__
+#	include <boxer/boxer.h>
+#endif
+
+#include <cstdio>
+#include <cstdlib>
+#include <exception>
+#include <spdlog/spdlog.h>
+
+#ifdef _WIN32
+#   include <minwindef.h>
+#endif
+
+namespace pxe {
+
+template<typename App_Type>
+	requires std::is_base_of_v<app, App_Type>
+auto run_app() -> int {
+	try {
+		App_Type application;
+		if(const auto error = application.run().unwrap(); error) {
+			SPDLOG_ERROR("{}", error->to_string());
+#ifndef __EMSCRIPTEN__
+			boxer::show(error->get_message().c_str(), "Error!", boxer::Style::Error);
+#endif
+			return EXIT_FAILURE;
+		}
+		return EXIT_SUCCESS;
+	} catch(const std::exception &e) {
+		std::fputs("unhandled exception in main: ", stderr);
+		std::fputs(e.what(), stderr);
+		std::fputc('\n', stderr);
+	} catch(...) {
+		std::fputs("unhandled non-standard exception in main\n", stderr);
+	}
+	return EXIT_FAILURE;
+}
+
+} // namespace pxe
+
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
+#define PXE_MAIN(AppClass)                                     \
+ auto main(int, char *[]) -> int {                             \
+  return pxe::run_app<AppClass>();                             \
+ }                                                             \
+                                                               \
+ IF_WIN32(                                                     \
+  auto WINAPI WinMain(                                         \
+   [[maybe_unused]] HINSTANCE hInstance,                       \
+   [[maybe_unused]] HINSTANCE hPrevInstance,                   \
+   [[maybe_unused]] LPSTR lpCmdLine,                           \
+   [[maybe_unused]] int nCmdShow) -> int {                     \
+   return pxe::run_app<AppClass>();                            \
+  }                                                            \
+ )
+
+#ifdef _WIN32
+#	define IF_WIN32(...) __VA_ARGS__ // NOLINT(cppcoreguidelines-macro-usage)
+#else
+#	define IF_WIN32(...) // NOLINT(cppcoreguidelines-macro-usage)
+#endif
